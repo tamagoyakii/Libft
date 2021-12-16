291bc40fe20b5aaa888aa41509fca4d570305419
tamagoyakii <rlawlgus4063@naver.com>
2021-12-16 16:45:37 +0900

    fix: norminette

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -6,26 +6,26 @@
 /*   By: jihyun <jihyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 15:44:19 by jihyun            #+#    #+#             */
-/*   Updated: 2021/12/16 15:58:58 by jihyun           ###   ########.fr       */
+/*   Updated: 2021/12/16 16:13:37 by jihyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    char    *ret;
-    int     i;
+	char			*ret;
+	unsigned int	i;
 
-    ret = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-    if (!ret)
-        return (0);
-    i = 0;
-    while (*(s + i))
-    {
-        *(ret + i) = (*f)(i, *(s + i));
-        i++;
-    }
-    *(ret + i) = 0;
-    return (ret);
+	ret = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!ret)
+		return (0);
+	i = 0;
+	while (*(s + i))
+	{
+		*(ret + i) = (*f)(i, *(s + i));
+		i++;
+	}
+	*(ret + i) = 0;
+	return (ret);
 }
