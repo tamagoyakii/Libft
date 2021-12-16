49bd95af12f1e65272170a239f4aa91b14cb2dbc
tamagoyakii <rlawlgus4063@naver.com>
2021-12-16 15:59:34 +0900

    feat: ft_strmapi.c

---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihyun <jihyun@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 15:44:19 by jihyun            #+#    #+#             */
+/*   Updated: 2021/12/16 15:58:58 by jihyun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *ret;
+    int     i;
+
+    ret = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+    if (!ret)
+        return (0);
+    i = 0;
+    while (*(s + i))
+    {
+        *(ret + i) = (*f)(i, *(s + i));
+        i++;
+    }
+    *(ret + i) = 0;
+    return (ret);
+}
