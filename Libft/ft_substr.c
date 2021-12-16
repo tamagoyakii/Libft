6beafb536dcc5c35086e6e3713d5b2bcc5348149
tamagoyakii <rlawlgus4063@naver.com>
2021-12-16 12:03:04 +0900

    fix: ft_substr.c

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jihyukim <jihyukim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jihyun <jihyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 17:34:48 by jihyukim          #+#    #+#             */
-/*   Updated: 2021/12/13 17:03:30 by jihyukim         ###   ########.fr       */
+/*   Updated: 2021/12/16 10:51:26 by jihyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (0);
+	if (ft_strlen(s) <= start)
+	{
+		ret = malloc(1);
+		*ret = 0;
+		return (ret);
+	}
 	if (ft_strlen(s + start) <= len)
 		len = ft_strlen(s + start);
 	ret = (char *)malloc(sizeof(char) * len + 1);
@@ -27,7 +33,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	while (*(s + start + i) && i < len)
 	{
-		*(ret + i) = *(s + start * i);
+		*(ret + i) = *(s + start + i);
 		i++;
 	}
 	*(ret + i) = 0;
