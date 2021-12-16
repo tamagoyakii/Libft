eac54752525459e431233478f19749167a5df40d
tamagoyakii <rlawlgus4063@naver.com>
2021-12-16 16:10:31 +0900

    fix: norminette

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,78 +6,69 @@
 /*   By: jihyun <jihyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 12:38:12 by jihyun            #+#    #+#             */
-/*   Updated: 2021/12/16 15:32:34 by jihyun           ###   ########.fr       */
+/*   Updated: 2021/12/16 16:09:16 by jihyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int  digit_count(int n)
+static int	digit_count(int n)
 {
-    int         count;
-    long long   m;
+	int			count;
+	long long	m;
 
-    count = 0;
-    m = n;
-    if (m < 0)
-    {
-        count++;
-        m *= -1;
-    }
-    while (m > 0)
-    {
-        count++;
-        m /= 10;
-    }
-    return (count);
+	count = 0;
+	m = n;
+	if (m < 0)
+	{
+		count++;
+		m *= -1;
+	}
+	while (m > 0)
+	{
+		count++;
+		m /= 10;
+	}
+	return (count);
 }
 
-static void digit_copy(char *ret, int n, int count)
+static void	digit_copy(char *ret, int n, int count)
 {
-    long long   m;
+	long long	m;
 
-    m = n;
-    if (m < 0)
-    {
-        *ret = '-';
-        m *= -1;
-    }
-    *(ret + count--) = 0;
-    while (m > 0)
-    {
-        *(ret + count) = m % 10 + 48;
-        m /= 10;
-        count--;
-    }
+	m = n;
+	if (m < 0)
+	{
+		*ret = '-';
+		m *= -1;
+	}
+	*(ret + count--) = 0;
+	while (m > 0)
+	{
+		*(ret + count) = m % 10 + 48;
+		m /= 10;
+		count--;
+	}
 }
 
-char    *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-    int         count;
-    char        *ret;
+	int		count;
+	char	*ret;
 
-    if (n == 0)
-    {
-        ret = (char *)malloc(sizeof(char) * 1 + 1);
-        *ret = '0';
-        *(ret + 1) = 0;
-    }
-    else
-    {
-        count = digit_count(n);
-        ret = (char *)malloc(sizeof(char) * digit_count(n) + 1);
-        if (!ret)
-            return (0);
-        digit_copy(ret, n, count);
-    }
-    return (ret);
-}
-
-#include <stdio.h>
-
-int main(void)
-{
-    int a = 10;
-    printf("%s\n", ft_itoa(a));
-    return (0);
+	if (n == 0)
+	{
+		ret = (char *)malloc(sizeof(char) * 1 + 1);
+		*ret = '0';
+		*(ret + 1) = 0;
+	}
+	else
+	{
+		count = digit_count(n);
+		ret = (char *)malloc(sizeof(char) * digit_count(n) + 1);
+		if (!ret)
+			return (0);
+		digit_copy(ret, n, count);
+	}
+	return (ret);
 }
