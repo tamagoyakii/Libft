d513add6b45c1d70c8c00785b81d52a3a0786403
tamagoyakii <rlawlgus4063@naver.com>
2022-01-12 15:19:49 +0900

    Libft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,31 +6,16 @@
 /*   By: jihyun <jihyun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 18:35:36 by jihyukim          #+#    #+#             */
-/*   Updated: 2022/01/12 15:00:31 by jihyukim         ###   ########.fr       */
+/*   Updated: 2022/01/12 15:19:06 by jihyun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	is_set(char const *set, char c)
-{
-	size_t	i;
-
-	i = 0;
-	while (*(set + i))
-	{
-		if (*(set + i) == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
 	size_t	end;
-	char	*ret;
 
 	if (!s1 && !set)
 		return (0);
@@ -39,13 +24,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1)
 		return (ft_strdup(""));
 	start = 0;
-	while (is_set(set, *(s1 + start)) && *(s1 + start))
+	while (ft_strchr(set, *(s1 + start)) && *(s1 + start))
 		start++;
 	end = ft_strlen(s1) - 1;
-	while (is_set(set, *(s1 + end)) && start < end)
+	while (ft_strchr(set, *(s1 + end)) && end)
 		end --;
-	if (start == end)
+	if (start > end)
 		return (ft_strdup(""));
-	ret = ft_substr(s1, start, end - start + 1);
-	return (ret);
+	return (ft_substr(s1, start, end - start + 1));
 }
